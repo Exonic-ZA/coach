035b1235299a059939f07420b53d266d1cec73ef
Jordan Levy <jordan@exonic.co.za>
2025-05-25 20:08:39 +0200

    fix: updated test case for button

--- spec/integration/homepage_spec.rb
+++ spec/integration/homepage_spec.rb
@@ -5,6 +5,7 @@ describe 'Homepage', :js, type: :feature do
     ENV['SLACK_CLIENT_ID'] = 'client_id'
     ENV['SLACK_CLIENT_SECRET'] = 'client_secret'
     ENV['SLACK_OAUTH_SCOPE'] = 'scope:read'
+    ENV['URL'] = 'http://localhost:5000'
     visit '/'
   end
 
@@ -12,13 +13,17 @@ describe 'Homepage', :js, type: :feature do
     ENV.delete 'SLACK_CLIENT_ID'
     ENV.delete 'SLACK_CLIENT_SECRET'
     ENV.delete 'SLACK_OAUTH_SCOPE'
+    ENV.delete 'URL'
   end
 
   it 'displays index.html page' do
     expect(title).to eq('Slava: Strava integration with Slack')
   end
 
-  it 'includes a link to add to slack with the client id' do
-    expect(find("a[href='https://slack.com/oauth/authorize?scope=scope:read&client_id=client_id']"))
+  it 'includes a link to add to slack with the client id and correct scope' do
+    expected_redirect_uri = CGI.escape("#{ENV['URL']}/slack/oauth/callback")
+    expected_href = "https://slack.com/oauth/v2/authorize?scope=scope:read&client_id=client_id&redirect_uri=#{expected_redirect_uri}"
+
+    expect(page).to have_selector("a[href='#{expected_href}']")
   end
 end
