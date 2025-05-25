9fefff39e2ae6b9ce31eb94030bfd3c7b7c11d32
Jordan Levy <jordan@exonic.co.za>
2025-05-25 23:03:59 +0200

    fix: tests

--- spec/integration/teams_spec.rb
+++ spec/integration/teams_spec.rb
@@ -1,11 +1,34 @@
 require 'spec_helper'
 
 describe 'Teams', :js, type: :feature do
+  let(:slack_response) do
+    {
+      ok: true,
+      access_token: 'token',
+      bot_user_id: 'bot_user_id',
+      team: {
+        id: 'team_id',
+        name: 'team_name'
+      },
+      authed_user: {
+        id: 'user_id',
+        access_token: 'user_token'
+      }
+    }.to_json
+  end
+
   before do
-    ENV['SLACK_CLIENT_ID'] = 'client_id'
-    ENV['SLACK_CLIENT_SECRET'] = 'client_secret'
-    ENV['SLACK_OAUTH_SCOPE'] = 'commands,chat:write,channels:read,groups:read,users:read,links:read,links:write'
-    ENV['SLACK_REDIRECT_URI'] = 'https://coach.exonic.co.za/teams'
+    stub_request(:post, 'https://slack.com/api/oauth.v2.access')
+      .with(body: hash_including(code: 'code'))
+      .to_return(status: 200, body: slack_response, headers: { 'Content-Type' => 'application/json' })
+
+    allow_any_instance_of(Slack::Web::Client).to receive(:conversations_open).with(users: 'user_id').and_return(
+      'channel' => { 'id' => 'C1' }
+    )
+
+    allow_any_instance_of(Slack::Web::Client).to receive(:chat_postMessage)
+    allow(SlackRubyBotServer::Service.instance).to receive(:create!)
+    allow_any_instance_of(Team).to receive(:ping!).and_return(ok: true)
   end
 
   after do
@@ -17,28 +40,6 @@ describe 'Teams', :js, type: :feature do
 
   context 'oauth', vcr: { cassette_name: 'auth_test' } do
     it 'registers a team' do
-      # Simulate a successful Slack OAuth v2 token exchange
-      slack_response = {
-        ok: true,
-        access_token: 'token',
-        bot_user_id: 'bot_user_id',
-        team: {
-          id: 'team_id',
-          name: 'team_name'
-        },
-        authed_user: {
-          id: 'user_id',
-          access_token: 'user_token'
-        }
-      }.to_json
-
-      stub_request(:post, 'https://slack.com/api/oauth.v2.access')
-        .with(body: hash_including(code: 'code'))
-        .to_return(status: 200, body: slack_response, headers: { 'Content-Type' => 'application/json' })
-
-      allow_any_instance_of(Team).to receive(:ping!).and_return(ok: true)
-      expect(SlackRubyBotServer::Service.instance).to receive(:create!)
-
       expect {
         visit '/?code=code'
         expect(page.find_by_id('messages')).to have_content 'Team successfully registered!'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,15 @@ require 'faker'
 require 'hyperclient'
 require 'webmock/rspec'
 
+# Load env helper before any specs run
+require_relative 'support/env_helper'
+
 ENV['RACK_ENV'] = 'test'
 
 require 'slack-ruby-bot/rspec'
 require 'slack-strava'
 
+# Load remaining support files (excluding env_helper to avoid double-require)
 Dir[File.join(File.dirname(__FILE__), 'support', '**/*.rb')].sort.each do |file|
-  require file
+  require file unless file.include?('env_helper.rb')
 end
