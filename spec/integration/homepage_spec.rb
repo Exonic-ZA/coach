8e6d718ab6b4c434546a93a854e0e041ff414524
Jordan Levy <jordan@exonic.co.za>
2025-05-25 21:30:16 +0200

    fix: remove callbacks from button

--- spec/integration/homepage_spec.rb
+++ spec/integration/homepage_spec.rb
@@ -5,7 +5,6 @@ describe 'Homepage', :js, type: :feature do
     ENV['SLACK_CLIENT_ID'] = 'client_id'
     ENV['SLACK_CLIENT_SECRET'] = 'client_secret'
     ENV['SLACK_OAUTH_SCOPE'] = 'scope:read'
-    ENV['URL'] = 'http://localhost:5000'
     visit '/'
   end
 
@@ -13,7 +12,6 @@ describe 'Homepage', :js, type: :feature do
     ENV.delete 'SLACK_CLIENT_ID'
     ENV.delete 'SLACK_CLIENT_SECRET'
     ENV.delete 'SLACK_OAUTH_SCOPE'
-    ENV.delete 'URL'
   end
 
   it 'displays index.html page' do
@@ -23,9 +21,7 @@ describe 'Homepage', :js, type: :feature do
   it 'includes a link to add to slack with the client id and correct scope' do
     expected_scope = ENV.fetch('SLACK_OAUTH_SCOPE')
     expected_client_id = ENV.fetch('SLACK_CLIENT_ID')
-    expected_redirect_uri = CGI.escape(ENV.fetch('URL'))
-
-    expected_href = "https://slack.com/oauth/v2/authorize?scope=#{expected_scope}&client_id=#{expected_client_id}&redirect_uri=#{expected_redirect_uri}"
+    expected_href = "https://slack.com/oauth/v2/authorize?scope=#{expected_scope}&client_id=#{expected_client_id}"
 
     expect(page).to have_link(href: expected_href)
   end
